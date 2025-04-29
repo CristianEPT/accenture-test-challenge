779135bc11247f15966c856a313e12742c07cd5c
Cristian <cristian-torres1@outlook.com>
2025-04-29 13:57:56 -0500

    add implement contract to franchises

---
+++ src/test/java/com/accenture/test/accenturetestchallenge/application/rest/FranchisesControllerTest.java
@@ -0,0 +1,86 @@
+package com.accenture.test.accenturetestchallenge.application.rest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.accenture.test.accenturetestchallenge.application.FranchiseRequest;
+import com.accenture.test.accenturetestchallenge.application.FranchiseResponse;
+import com.accenture.test.accenturetestchallenge.domain.model.Franchise;
+import com.accenture.test.accenturetestchallenge.domain.ports.FranchisePort;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(controllers = FranchisesController.class)
+class FranchisesControllerTest {
+
+  @Autowired private WebTestClient webTestClient;
+
+  @MockitoBean private FranchisePort franchisePort;
+
+  @Test
+  void createFranchise_whenAllSuccess() {
+    FranchiseRequest request = new FranchiseRequest();
+    request.setName("Pizzeria");
+
+    Franchise franchise = new Franchise("123", "Pizzeria");
+
+    Mockito.when(franchisePort.createFranchise("Pizzeria")).thenReturn(Mono.just(franchise));
+
+    webTestClient
+        .post()
+        .uri("/franchises")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isCreated()
+        .expectBody(FranchiseResponse.class)
+        .value(
+            response -> {
+              assertThat(response.getId()).isEqualTo("123");
+              assertThat(response.getName()).isEqualTo("Pizzeria");
+            });
+  }
+
+  @Test
+  void createFranchise_whenNameIsNull_returnsError() {
+    FranchiseRequest request = new FranchiseRequest();
+
+    webTestClient
+        .post()
+        .uri("/franchises")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .is4xxClientError()
+        .expectBody()
+        .consumeWith(result -> assertThat(result.getResponseBody()).isNotNull());
+  }
+
+  @Test
+  void createFranchise_whenFailed_returnsError() {
+
+    FranchiseRequest request = new FranchiseRequest();
+    request.setName("Pizzeria");
+
+    Mockito.when(franchisePort.createFranchise("Pizzeria"))
+        .thenThrow(new RuntimeException("error saving franchise"));
+
+    webTestClient
+        .post()
+        .uri("/franchises")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .is5xxServerError()
+        .expectBody()
+        .consumeWith(result -> assertThat(result.getResponseBody()).isNotNull());
+  }
+}
